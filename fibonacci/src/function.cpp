7558319d5ad505ed2d7c0588c66c5c6451ca875c
Danilo25 <danilo25aciole@gmail.com>
2021-07-11 15:38:33 -0300

    clean

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -3,23 +3,4 @@
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    vector<unsigned int> fibo={};
-    vector<unsigned int>::iterator somar;
-    unsigned int val=1;
-
-    if(n>1){
-        fibo.push_back(val);
-        while (val<n)
-        {
-            fibo.push_back(val);
-            val = *fibo.end()-1 + *fibo.end()-2;
-        }
-        for (auto i=fibo.begin(); i != fibo.end(); i++)
-        {
-            std::cout<< *i << " " <<std::endl;
-        }
-        return std::vector<unsigned int>{};
-    }else{
-        return std::vector<unsigned int>{};
-    }
 }
