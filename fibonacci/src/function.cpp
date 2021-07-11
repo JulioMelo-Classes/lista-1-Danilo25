df5517fe3d5e51a9e89b8f419ee1e06053d889d1
Danilo25 <danilo25aciole@gmail.com>
2021-07-11 15:03:41 -0300

    MinMax

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,25 @@
 #include "function.h"
+#include <iostream>
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    // TODO: adicione o seu código aqui.
+    vector<unsigned int> fibo={};
+    vector<unsigned int>::iterator somar;
+    unsigned int val=1;
 
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    if(n>1){
+        fibo.push_back(val);
+        while (val<n)
+        {
+            fibo.push_back(val);
+            val = *fibo.end()-1 + *fibo.end()-2;
+        }
+        for (auto i=fibo.begin(); i != fibo.end(); i++)
+        {
+            std::cout<< *i << " " <<std::endl;
+        }
+        return std::vector<unsigned int>{};
+    }else{
+        return std::vector<unsigned int>{};
+    }
 }
--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -12,8 +12,29 @@
 
 std::pair<int,int> min_max( int V[], size_t n )
 {
-    // TODO: Adicione aqui sua solução.
+    int min=V[0],
+        max=V[0],
+        posMin=0,
+        posMax=0;
 
+    if(n<1){
+        posMin=-1;
+        posMax=-1;
+    }
+    else{
+        for(int i=0;i<n;i++){
+            if(V[i]<min){
+                posMin = i;
+                min = V[i];
+            }
+            if(V[i]>max){
+                posMax = i;
+                max = V[i];
+            }else if(V[i]==max){
+                posMax = i;
+            }
+        }
+    }
     // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+    return { posMin, posMax };
 }
